210ae9c6bd76278509d9101fac85bdbb85a085ef
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2026-04-13 12:31:29 +0100

    logo changed in the footer

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Logo({ light = false }: { light?: boolean }) {
   return (
     <Link href="/" className={`flex items-center gap-2 ${light ? "text-white" : "text-charcoal"}`}>
-      <Image src="/Stratena.png" alt="Stratena Logo" width="180" height="20" />
+      <Image src={`${light? '/Stratena-light.png':'/Stratena.png'}`} alt="Stratena Logo" width="180" height="20" />
     </Link>
   );
 }
